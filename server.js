6313ebe423a503d97cfcb17c790677969e6ee2e1
nor0220 <maraznori@gmail.com>
2022-01-11 15:20:49 +0100

    code organizing

--- server.js
+++ server.js
@@ -20,9 +20,20 @@ app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
 
+// Setup Server
+const port = 8080;
+const server = app.listen(port, listening);
+
+//Callback to debug
+function listening(){
+    console.log('server running');
+    console.log(`running on localhost: ${port}`);
+};
+
 //GET route that returns the projectData
 app.get('/getData', function (req, res) {
     res.send(projectData);
+    console.log('server side getData:', projectData);
 });
 
 //POST route that adds incoming data to projectData
@@ -33,16 +44,7 @@ app.post('/addData', function addData (req, res){
         user_response: req.body.user_response
       };
     
-      projectData.push(newEntry);
-      console.log('server side data:', projectData);
+      Object.assign(projectData, newEntry);
+      res.send(projectData);
+      console.log('server side addData:', projectData);
 });
-
-// Setup Server
-const port = 8080;
-const server = app.listen(port, listening);
-
-//Callback to debug
-function listening(){
-    console.log('server running');
-    console.log(`running on localhost: ${port}`);
-};
--- website/app.js
+++ website/app.js
@@ -2,9 +2,24 @@
 let baseURL = 'https://api.openweathermap.org/data/2.5/weather?';
 const apiKey = '&appid=519d637a730bd9233909f0115a0cf8c5&units=imperial';
 
-// Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+document.getElementById('generate').addEventListener('click', performAction);
+
+function performAction(e){
+    let newData =  document.getElementById('feelings').value;
+    let zip='';
+    if(document.getElementById('zip').value!=''){
+        zip =  'zip=' + document.getElementById('zip').value + ',us';
+    }else{
+        zip = 'zip=10001,us'; //default newyork
+    }
+
+    console.log('url: ', baseURL + zip + apiKey);
+    getData(baseURL, zip, apiKey)
+    .then(function(data){
+        postData('/addData', {temperature :data.main.temp, date: data.dt, user_response:newData} );
+        updateUI()
+    })
+};
 
 //POST data
 const postData = async ( url = '', data = {})=>{
@@ -41,31 +56,11 @@ const updateUI = async () => {
     const request = await fetch('/getData');
     try{
       const allData = await request.json();
-      document.getElementById('date').innerHTML = new Date(allData[0].date * 1000);
-      document.getElementById('temp').innerHTML = Math.round(allData[0].temperature)+ 'degrees';
-      document.getElementById('content').innerHTML = allData[0].user_response;
+      console.log("UpdatUI: ", allData);
+      document.getElementById('date').innerHTML = new Date(allData.date * 1000);
+      document.getElementById('temp').innerHTML = Math.round(allData.temperature)+ ' degrees';
+      document.getElementById('content').innerHTML = allData.user_response;
     }catch(error){
       console.log("UpdatUI error", error);
     }
   }
-
-document.getElementById('generate').addEventListener('click', performAction);
-
-function performAction(e){
-    let newData =  document.getElementById('feelings').value;
-    let zip='';
-    if(document.getElementById('zip').value){
-        zip =  'zip=' + document.getElementById('zip').value + ',us';
-    }else{
-        zip = 'zip=10001,us'; //default newyork
-    }
-    
-    getData(baseURL, zip, apiKey)
-    .then(function(data){
-        console.log(data);
-        postData('/addData', {temperature :data.main.temp, date: data.dt, user_response:newData} );
-    })
-    .then(
-        updateUI()
-    )
-};
